0314b6fc8d007c4976a8e20cb9beeefef5b7fa10
John Fellman <john.fellman@avrs.com>
2015-08-03 09:09:59 -0700

    adds working second endpoint

--- app/controllers/stat_controller.rb
+++ app/controllers/stat_controller.rb
@@ -18,8 +18,32 @@ class StatController < ApplicationController
       day = Stat.mysql_datestamp(i)
       day_start = "#{day} 00:00:00"
       day_end   = "#{day} 23:59:59"
-      result    = Stat.with_sql("select url, count(url) as visits from stats where created_at >= \'#{day_start}\' and created_at <= \'#{day_end}\' group by url order by visits desc")
-      results[day] = result.all
+      result    = Stat.with_sql(<<-sql
+        select 
+          s.url, sub.visits, referrer, count(referrer) as refer_visits 
+        from 
+          (select url, count(url) as visits from stats  group by url order by count(url) desc limit 10) sub         
+        join                         
+          stats s on sub.url=s.url 
+        where 
+          created_at >= '#{day_start}' and created_at <= '#{day_end}' 
+        group by 
+          s.url, s.referrer;
+      sql
+      )
+
+      nested_result = {}
+      rows = result.all
+
+      rows.each do |row|
+        unless nested_result[row.url]
+          nested_result[row.url] = { :visits => row[:visits], :referrers => [] }
+        end
+        parent_row = nested_result[row.url]
+        nested_result[row.url][:referrers].push({ url: row.referrer, visits: row[:refer_visits] })
+      end
+
+      results[day] = nested_result
     end
 
     render json: results
